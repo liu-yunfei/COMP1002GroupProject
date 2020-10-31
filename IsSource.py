ca70d6d9dd60e0eda06704c20f1d9b962771133c
liu-yunfei <jasonliuyf@gmail.com>
2020-10-31 19:06:58 +0800

    Move the directsource function into issourcefunction

--- IsSource.py
+++ IsSource.py
@@ -1,23 +1,23 @@
 """This function is used to check whether the given sourcepost is the source of a report.
+It receives sourcepost and reportpost titles and return True when sourcepost is source of reportpost
+return False otherwise.
 
-Written by Muyuan LI"""
-
-
-def DirectSource(post):
-    try:
-        postData = open('post/' + post + '.txt', encoding='UTF-8')
-    except IOError:
-        print('Fail to open the file\n')
-        return 'Fail'
-    line1 = postData.readline()
-    line2 = postData.readline()
-    line3 = postData.readline()
-    quotation = line3.strip()
-    postData.close()
-    return quotation
-
+Written by Muyuan LI Oct 30, 2020
+Edited by Yunfei LIU Oct 31, 2020"""
 
 def IsSource(sourcePost, reportPost):
+    def DirectSource(post):
+        try:
+            postData = open('post/' + post + '.txt', encoding='UTF-8')
+        except IOError:
+            print('Fail to open the file\n')
+            return 'Fail'
+        line1 = postData.readline()
+        line2 = postData.readline()
+        line3 = postData.readline()
+        quotation = line3.strip()
+        postData.close()
+        return quotation
     #This part is used to ensure the file names are valid
     try:
         fin1 = open('post/' + sourcePost + '.txt', encoding='UTF-8')
