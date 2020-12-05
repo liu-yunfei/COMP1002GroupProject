a4a15de78a7d54faa0105817e39946b384d5d7fd
liu-yunfei <jasonliuyf@gmail.com>
2020-12-05 21:08:40 +0800

    Edit Anchor() and sync to GUI.py

--- Anchor.py
+++ Anchor.py
@@ -2,37 +2,29 @@
 A file of  post data will be read into the function, it returns the article K,
 if it is the anchor of article A, i.e. K is an anchor and A directly/indirectly quote K 
 
-Written by Owen CHAN  15 Nov, 2020 """
+Written by Owen CHAN  15 Nov, 2020 
+Edited by Yunfei LIU Dec 5, 2020"""
 
 def Anchor(reportPost):
-
+    def GetSource(report):
         try:
-            postData = open( 'post/' +  reportPost + '.txt' , encoding = 'UTF-8')
+            postData = open( 'post/' +  report + '.txt' , encoding = 'UTF-8')
         except IOError :   
             print('Fail to open the file\n')
             return 'Fail'
-
         line1 = postData.readline()   
         line2 = postData.readline()
         line3 = postData.readline()
         quote = line3.strip()
         postData.close()
+        return  quote
 
-        if quote = 'null' :
-            print(reportPost, 'does not quote any other post.')
-            return False
-        else:
-            try:
-                postData = open('post/' + quote + '.txt',  encoding = 'UTF-8')
-            except IOError :
-                print('Fail to open to quoted file\n')
-                return 'Fail'
-            line1 = postData.readline()   
-            line2 = postData.readline()
-            line3 = postData.readline()
-            quote2 = line3.strip()
+    report = reportPost
+    while True:
+        source = GetSource(report)
+        if source == 'null':
+            return report
+        if source == 'Fail':
+            return ''
+        report = source
 
-        if  quote2 = 'null' :
-            print(quote2, 'is an anchor of ', reportPost)
-            return True
-        
