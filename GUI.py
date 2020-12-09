01d920aa123ffab5c9afd8436482defd9d1d55e9
liu-yunfei <jasonliuyf@gmail.com>
2020-12-09 14:40:27 +0800

    add find friendship between kols function

--- GUI.py
+++ GUI.py
@@ -187,6 +187,7 @@ def GUI():
             qIndex.mainloop()
 
         def GUIFindKOL():
+            import tkinter.messagebox
             kol = tk.Tk()
             kol.title("Find KOL")
             kol.geometry("600x400")
@@ -240,8 +241,20 @@ def GUI():
                 for item in kolList:
                     GUIFindKOL.rightBox.insert("end", item)
 
+            def findFriendship():
+                kolList = KOL(GUIFindKOL.impactNumber, GUIFindKOL.percentNumber)
+                for i in range(len(kolList)):
+                    kolList[i] = kolList[i][0]
+                friendshipList = KOLFriendship(kolList)
+                msgStr = ''
+                for item in friendshipList:
+                    msgStr += item[0] + '-----' + item[1] + '\n'
+                tkinter.messagebox.showinfo(title = "Friendship between KOLs",message = msgStr)
+
             emptyLabel2 = tk.Label(kol, text=" ", font=("Arial", 16), height=1).pack()
-            findButton = tk.Button(kol, text="Find KOL", command=addKOL, font=("Arial", 16), height=2, width=20,
+            findButton = tk.Button(kol, text="Find KOL", command=addKOL, font=("Arial", 16), height=1, width=20,
+                                   bg="light grey").pack()
+            friendButton = tk.Button(kol, text="Find KOL Friendship", command=findFriendship, font=("Arial", 16), height=1, width=20,
                                    bg="light grey").pack()
 
             kol.mainloop()
@@ -1778,6 +1791,33 @@ def KOL(miniIndex, percentage):
             kolList.append(impactList[i])
     return kolList
 
+"""This function is used for find the friendship in KOLs
+it receives a list of users and return a list of friends tuples
+
+Written by Yunfei LIU Dec 9, 2020"""
+
+def KOLFriendship(list):
+    # Get Data
+    user = open("user.txt")
+    userInfoList = []
+    for userInfo in user:
+        userInfoList.append(userInfo.strip())
+    nameList = []
+    friendList = []
+    friendshipSet = set()
+    for userInfo in userInfoList:
+        name, firstname, lastname, password, birthday, phoneNumber, friend, post = map(str, userInfo.split(","))
+        nameList.append(name)
+        friendList.append(friend.split(';'))
+    
+    #Find the relationship between KOLs   
+    for username in list:
+        if nameList.index(username) != -1:
+            for friend in friendList[nameList.index(username)]:
+                if (friend in list) and ((friend,username) not in friendshipSet):
+                    friendshipSet.add((username,friend))
+    return friendshipSet
+
 
 """This function is main function in terminal.
 It receives void and return 0 when no error
@@ -1801,30 +1841,48 @@ def main():
                 if funcChar == "R":
                     break
                 if funcChar == "A":
-                    print("\n1.UserImpactIndex\n2.PostImpactIndex\n3.FriendshipIndex\n4.QuotationIndex\n5.FindKOL\n")
+                    print("\n1.UserImpactIndex\n2.PostImpactIndex\n3.FriendshipIndex\n4.QuotationIndex\n5.FindKOL\n6.Find KOLs' Friendship")
                     choiceChar = str(input("Please enter numbers to use functions, other to return: "))
                     if choiceChar == "1":
                         userName = str(input("\nEnter the user name: "))
                         print("The impact index for %s is %s\n" % (userName, UserImpactIndex(userName)))
+                        print("----------END----------\n")
                     elif choiceChar == "2":
                         post = str(input("\nEnter the post title: "))
                         print("The impact index for %s is %s\n" % (post, PostImpactIndex(post)))
+                        print("----------END----------\n")
                     elif choiceChar == "3":
                         userName1 = str(input("\nEnter user name 1: "))
                         userName2 = str(input("\nEnter user name 2: "))
                         print("\nThe friendship index between %s and %s is %s" % (
                         userName1, userName2, FriendshipIndex(userName1, userName2)))
+                        print("----------END----------\n")
                     elif choiceChar == "4":
                         userName1 = str(input("\nEnter user name 1: "))
                         userName2 = str(input("\nEnter user name 2: "))
                         print("\nThe quotation index between %s and %s is %s" % (
                         userName1, userName2, QuotationIndex(userName1, userName2)))
+                        print("----------END----------\n")
                     elif choiceChar == "5":
                         miniIndex = float(input("Please enter the minimum index of KOL: "))
                         percentage = float(input("Please enter the percentage of KOL: "))
                         print("\nThe KOL list\n")
                         for i in KOL(miniIndex, percentage):
                             print("Name:%s\nImpact Index:%s\n" % (i[0], i[1]))
+                            print("----------END----------\n")
+                    elif choiceChar == "6":
+                        miniIndex = float(input("Please enter the minimum index of KOL: "))
+                        percentage = float(input("Please enter the percentage of KOL: "))
+                        print("\nThe KOL list\n")
+                        KOLnameList = []
+                        for i in KOL(miniIndex, percentage):
+                            print("Name:%s\nImpact Index:%s\n" % (i[0], i[1]))
+                            KOLnameList.append(i[0])
+                        friendship = KOLFriendship(KOLnameList)
+                        print("\n\nThe friendship between KOLs are as follows\n")
+                        for item in friendship:
+                            print("%s -------- %s" %(item[0],item[1]))
+                        print("----------END----------\n")
 
                 if funcChar == "B":
                     print(
@@ -1834,12 +1892,14 @@ def main():
                     if choiceChar == "1":
                         post = str(input("Please enter the post title: "))
                         print("The anchor of post '%s' is '%s'\n" %(post,Anchor(post)))
+                        print("----------END----------\n")
                     elif choiceChar == "2":
                         post = str(input("\nEnter the post title: "))
                         reportList = DirectReport(post)
                         print("The following are the direct reports of post '%s'" % post)
                         for item in reportList:
                             print(item)
+                        print("----------END----------\n")
                     elif choiceChar == "3":
                         GetA()
                     elif choiceChar == "4":
@@ -1849,28 +1909,34 @@ def main():
                         post2 = str(input("\nEnter report title: "))
                         print("%s is the direct source of %s ? " % (post1, post2), end='')
                         print(IsDirectSource(post1, post2))
+                        print("----------END----------\n")
                     elif choiceChar == "6":
                         post1 = str(input("\nEnter source title: "))
                         post2 = str(input("\nEnter report title: "))
                         print("%s is the source of %s ? " % (post1, post2), end='')
                         print(IsSource(post1, post2))
+                        print("----------END----------\n")
                     elif choiceChar == "7":
                         userName1 = str(input("\nEnter user name 1: "))
                         userName2 = str(input("\nEnter user name 2: "))
                         print("%s and %s are friends? " % (userName1, userName2), end='')
                         print(IsFriend(userName1, userName2))
+                        print("----------END----------\n")
                     elif choiceChar == "8":
                         post = str(input("\nEnter the post title: "))
                         NicePrintA(post)
+                        print("----------END----------\n")
                     elif choiceChar == "9":
                         userName = str(input("\nEnter the user name: "))
                         NicePrintU(userName)
+                        print("----------END----------\n")
                     elif choiceChar == "10":
                         post = str(input("\nEnter the post title: "))
                         reportList = Report(post)
                         print("The following are the reports of post '%s'" % post)
                         for item in reportList:
                             print(item)
+                        print("----------END----------\n")
 
 
 main()
